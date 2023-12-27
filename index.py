ccf2b1f1561f690792aaa9f1080c8ad9c767d222
Qudils <arcopintolorenzo01@gmail.com>
2023-12-27 19:06:54 +0100

    versione 27/12/2023

--- index.py
+++ index.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_session import Session
 from pymongo import MongoClient
-
+from datetime import datetime
 
 app = Flask(__name__, template_folder='../Myst', static_folder='static')
 app.secret_key = 'your_secret_key'  # Cambia con una chiave segreta più sicura
 client = MongoClient('mongodb://localhost:27017')
 db = client.pymongo
 usersCollection = db.users
+
+# Configura Flask-Session
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 @app.route('/')
 @app.route('/main')
@@ -16,7 +20,6 @@ def main():
 
 @app.route('/index.html')
 def index():
-
     return render_template('index.html')
 
 @app.route('/signup.html', methods=['GET', 'POST'])
@@ -29,7 +32,6 @@ def signup():
             flash(request.form['username'] + ' username is already exist')
             return redirect(url_for('signup'))
 
-    
         users.insert_one({'username': request.form['username'], 'password': request.form['password'], 'email': request.form['email']})
 
         session['username'] = request.form['username']
@@ -37,22 +39,43 @@ def signup():
 
     return render_template('signup.html')
 
-
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         users = db.users
-        signin_user = users.find_one({'username': request.form['username'],'password':request.form['password']})
+        signin_user = users.find_one({'username': request.form['username'], 'password': request.form['password']})
 
         if signin_user:
             session['username'] = request.form['username']
-            return redirect(url_for('index'))
+            return redirect(url_for('utente'))  # Reindirizza a utente.html dopo il login
 
         flash('Username and password combination is wrong')
-        return url_for('index')
 
     return render_template('login.html')
 
+@app.route('/utente')
+def utente():
+    # Controlla se l'utente è autenticato prima di visualizzare la pagina utente
+    if 'username' in session:
+        username = session['username']
+
+        # Ora dovresti interrogare il database per ottenere le informazioni dell'utente
+        users = db.users
+        user_data = users.find_one({'username': username})
+
+        if user_data:
+            email = user_data.get('email', 'Email non disponibile')
+            data_registrazione = user_data.get('registration_date', datetime.now().strftime('%Y-%m-%d'))
+            stato_utente = user_data.get('status', 'Stato non disponibile')
+
+            return render_template('utente.html', username=username, email=email, data_registrazione=data_registrazione, stato_utente=stato_utente)
+        else:
+            flash('Utente non trovato nel database.')
+            return redirect(url_for('login'))
+    else:
+        flash('Devi effettuare il login per accedere a questa pagina.')
+        return redirect(url_for('login'))
+    
 @app.route('/giocoAce')
 def Ace():
     return render_template('giocoAce.html')
