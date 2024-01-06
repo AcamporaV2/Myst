48b7284a827abe72b1beb20d5b13b855ff5f0f1f
Acampora <92322511+AcamporaV2@users.noreply.github.com>
2024-01-06 11:53:22 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,3 @@
-//Makes the bottom bar follow the screen when scrolling
-window.addEventListener('scroll', function() {
-  var footer = document.querySelector('.footer');
-    footer.style.transition = 'bottom 0.5s';
-
-  if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
-    footer.style.bottom = '0';
-  } else {
-    footer.style.bottom = '-' + footer.offsetHeight + 'px';
-  }
-});
-
 
 //function to get the price for the games
 function getPrezzo(oggetto) {
